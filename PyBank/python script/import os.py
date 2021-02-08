f5784e9130e87360bc3dafee7c1e37aac6f24dc0
maxe23 <maxeiteljorg@gmail.com>
2021-02-07 19:10:27 -0500

    corrections

--- PyBank/python script/import os.py
+++ PyBank/python script/import os.py
@@ -2,7 +2,7 @@ import os
 import csv
 
 
-budget_data = os.path.join('..','resources','budget_data.csv')
+budget_data = os.path.join('..','Resources','budget_data.csv')
 
 
 # Read in the CSV file
@@ -19,15 +19,22 @@ with open(budget_data, 'r') as csvfile:
     date=[]
     total=[]
     change=[]
+    previous_profit= 0
 
-    for row in csv_reader:
+    for i, row in enumerate(csv_reader):
         #print (row)
         profit=(row[1])
 
         column2.append(float(profit))
         date.append((row[0]))
+        if i !=0:
+
+            change.append(int(profit)-int(previous_profit))
+            previous_profit=(row[1])
+        else:
+            previous_profit=(row[1])
     
-    for profit in column2:
+    #for profit in column2:
 
         
 
@@ -40,9 +47,9 @@ with open(budget_data, 'r') as csvfile:
 
     print(f'Total Months: {len(date)}')  
     print(f'Total:', sum(column2))
-    print(f'Average Change:', sum(column2)/len(column2))
-    print(f"Greatest Increase in Profits:", max(column2))
-    print(f'Greatest Decrease In Profits:', min(column2))
+    print(f'Average Change:', sum(change)/len(change))
+    print(f"Greatest Increase in Profits:", max(change))
+    print(f'Greatest Decrease In Profits:', min(change))
 
 
 
@@ -58,8 +65,8 @@ with open(budget_data, 'r') as csvfile:
 f =  open("output.txt", "w")
 print(f'Total Months: {len(date)}', file= f)  
 print(f'Total:', sum(column2), file= f)
-print(f'Average Change:', sum(column2)/len(column2), file= f)
-print(f"Greatest Increase in Profits:", max(column2), file= f)
-print(f'Greatest Decrease In Profits:', min(column2), file= f)
+print(f'Average Change:', sum(change)/len(change), file= f)
+print(f"Greatest Increase in Profits:", max(change), file= f)
+print(f'Greatest Decrease In Profits:', min(change), file= f)
 
 f.close()
